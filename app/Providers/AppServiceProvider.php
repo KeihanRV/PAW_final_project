8647caff4e86810064ae0ee064651f4687ea0c2b
KeihanRV <zenzuken56@student.ub.ac.id>
2025-11-24 06:48:24 +0700

    fix: konten tidak load dengan https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    if ($this->app->environment('production', 'staging', 'development')) {
+        // Force semua skema URL yang dihasilkan menjadi HTTPS
+        URL::forceScheme('https');
+    }
     }
 }
